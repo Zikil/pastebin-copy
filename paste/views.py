4c17a6c714e778f8f9bd5691afd8643ff60f9fa4
zik <il.zahvatkin33@gmail.com>
2019-08-24 20:37:16 +0700

    paginator

--- paste/views.py
+++ paste/views.py
@@ -5,12 +5,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 from datetime import datetime
 import datetime
 
-from .models import *
-from .forms import *
+from .models import Paste
+from .forms import PasteForm
 
 # pasteslist служит для списка паст для постоянного отображения на сайте
 # Create your views here.
@@ -20,7 +21,7 @@ def pasteslist_get():  #список паст для постоянного от
 
 
 def paste_list(request):  # вьюха для просмотра всех доступных паст
-    pastes = Paste.objects.filter(Q(die_time__gt=datetime.datetime.now()) | Q(die_time__isnull=True))
+    pastes = Paste.objects.filter(Q(die_time__gt=datetime.datetime.now()) | Q(die_time__isnull=True) | Q(access='public'))
 
     search_query = request.GET.get('search', '')
     if search_query:
@@ -28,6 +29,22 @@ def paste_list(request):  # вьюха для просмотра всех дос
     else:
         pastes = pastes.all()
 
+    paginator = Paginator(pastes, 10) # Show 10 contacts per page
+
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
 
     lenpaste = pastes.count()  # количество паст
     pasteslist = pasteslist_get()
@@ -35,7 +52,13 @@ def paste_list(request):  # вьюха для просмотра всех дос
         'search_query': search_query,
         'pasteslist': pasteslist,
         'pastes': pastes,
-        'lenpaste': lenpaste
+        'lenpaste': lenpaste,
+        # 'contacts': contacts,
+
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prev_url': prev_url
     }
     return render(request, 'paste/pastes_list.html', context=context)
 
@@ -58,6 +81,7 @@ class PasteCreate(View):  # вьюха для создания новой пас
             if request.user.is_authenticated:
                 new_paste.user = request.user
                 new_paste.save()
+
             return redirect(new_paste)
         pasteslist = pasteslist_get()
         context = {
@@ -87,7 +111,8 @@ class PasteDetail(View):  # вьюха для просмотра отдельн�
                 return render(request, 'paste/paste_detail.html', context=context)
 
 
-class PasteDelete(View):  # вьюха для удаления пасты
+class PasteDelete(LoginRequiredMixin, View):  # вьюха для удаления пасты
+    raise_exception = True
     def get(self, request, slug):
         paste = get_object_or_404(Paste, slug__iexact=slug)
         pasteslist = pasteslist_get()
@@ -101,3 +126,20 @@ class PasteDelete(View):  # вьюха для удаления пасты
         paste = get_object_or_404(Paste, slug__iexact=slug)
         paste.delete()
         return redirect(reverse('paste_list_url'))
+
+
+class MyPaste(LoginRequiredMixin, View):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
+    def get(self, request):
+        pastes = Paste.objects.filter(Q(die_time__gt=datetime.datetime.now()) | Q(die_time__isnull=True))
+        pastes = pastes.filter(user=request.user)
+
+        lenpaste = pastes.count()
+        pasteslist = pasteslist_get()
+        context = {
+            'pasteslist': pasteslist,
+            'pastes': pastes,
+            'lenpaste': lenpaste,
+        }
+        return render(request, 'paste/my_pastes_list.html', context=context)
